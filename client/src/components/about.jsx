6fdc54a5edc2762f7ee5db426d100f531688b0be
prathmeshkulakrni <prathmeshkulkarni093@gmail.com>
2026-04-22 23:52:59 +0530

    Initial commit with project updates

---
+++ client/src/components/about.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material"
+import { Box, Container } from "@mui/system"
+import Copyright from "./Copyright"
+
+const About = () => {
+  return (
+    <>
+      Jai Shree Ram
+    </>
+  )
+}
+
+export default About
